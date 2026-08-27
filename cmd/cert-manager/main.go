b10d6b98b592765080f4637110dbfd91b9233fae
Andreas <andreasmagos@gmail.com>
2026-08-27 22:58:37 +0300

    feat: add new content

---
+++ cmd/cert-manager/main.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"os"
+	"time"
+)
+
+type CertInfo struct {
+	Hostname  string    `json:"hostname"`
+	Issuer    string    `json:"issuer"`
+	Expires   string    `json:"expires"`
+	DaysLeft  int       `json:"days_left"`
+	Status    string    `json:"status"`
+}
+
+func main() {
+	hostCmd := flag.NewFlagSet("check", flag.ExitOnError)
+	hostHostname := hostCmd.String("host", "", "Hostname to check")
+	hostPort := hostCmd.Int("port", 443, "Port to check")
+
+	listCmd := flag.NewFlagSet("list", flag.ExitOnError)
+	listFile := listCmd.String("file", "", "File with hosts (one per line)")
+
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: cert-manager <command> [arguments]")
+		fmt.Println("Commands: check, list")
+		os.Exit(1)
+	}
+
+	switch os.Args[1] {
+	case "check":
+		hostCmd.Parse(os.Args[2:])
+		if *hostHostname == "" {
+			fmt.Println("Error: --host is required")
+			os.Exit(1)
+		}
+		checkCert(*hostHostname, *hostPort)
+	case "list":
+		listCmd.Parse(os.Args[2:])
+		if *listFile == "" {
+			fmt.Println("Error: --file is required")
+			os.Exit(1)
+		}
+		listCerts(*listFile)
+	default:
+		fmt.Printf("Unknown command: %s\n", os.Args[1])
+		os.Exit(1)
+	}
+}
+
+func checkCert(hostname string, port int) {
+	addr := fmt.Sprintf("%s:%d", hostname, port)
+	conn, err := tls.Dial("tcp", addr, &tls.Config{InsecureSkipVerify: true})
+	if err != nil {
+		fmt.Printf("Error connecting to %s: %v\n", addr, err)
+		os.Exit(1)
+	}
+	defer conn.Close()
+
+	certs := conn.ConnectionState().PeerCertificates
+	if len(certs) == 0 {
+		fmt.Println("No certificates found")
+		os.Exit(1)
+	}
+
+	cert := certs[0]
+	daysLeft := int(time.Until(cert.NotAfter).Hours() / 24)
+	status := "OK"
+	if daysLeft <= 0 {
+		status = "EXPIRED"
+	} else if daysLeft <= 7 {
+		status = "CRITICAL"
+	} else if daysLeft <= 30 {
+		status = "WARNING"
+	}
+
+	info := CertInfo{
+		Hostname: hostname,
+		Issuer:   cert.Issuer.CommonName,
+		Expires:  cert.NotAfter.Format("2006-01-02"),
+		DaysLeft: daysLeft,
+		Status:   status,
+	}
+
+	data, _ := json.MarshalIndent(info, "", "  ")
+	fmt.Println(string(data))
+}
+
+func listCerts(filename string) {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		fmt.Printf("Error reading file: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Simple line-by-line parsing
+	_ = data
+	fmt.Println("List certs from file - implement parsing")
+}
